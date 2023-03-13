b535a7c7f9ad2fade2f46d032787ae1dbbf63480
acerlop <acerlop430@g.educaand.es>
2023-03-13 22:44:41 +0100

    Script PlayerPosition

---
+++ Assets/Scripts/PlayerPosition.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPosition : MonoBehaviour
+{
+    public GameObject Player;
+    public Transform positionRoom;
+    public Transform positionKitchen;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Room()
+    {
+        Player.transform.position = positionRoom.position;
+    }
+
+    public void Kitchen()
+    {
+        Player.transform.position = positionKitchen.position;
+    }
+}
